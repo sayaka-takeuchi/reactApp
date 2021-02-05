59de3823c4a711dfed7e01c3a23bc52106cdf25f
sayaka-takeuchi <ars0915.cute0910@gmail.com>
2021-02-05 14:39:41 +0900

    useStateを使ってcountボタンを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { FC } from "react";
+import React, { useState } from "react";
 
-type AppPropsType = {
-  text: string;
-}
-
-const App: FC<AppPropsType> = (props) => {
+const App = () => {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-       <header className="App-header">
-         <img src={logo} className="App-logo" alt="logo" />
-         <p>
-           Edit <code>src/App.js</code> and save to reload.
-         </p>
-         <a
-           className="App-link"
-           href="https://reactjs.org"
-           target="_blank"
-           rel="noopener noreferrer"
-         >
-           {props.text}
-         </a>
-       </header>
-     </div>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <span>{count}</span>
+      <button onClick={() => setCount(count - 1)}>-</button>
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App text={"succless!!!"}/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
