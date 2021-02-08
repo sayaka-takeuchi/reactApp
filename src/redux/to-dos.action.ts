75232383c3b5c1a4833715b52214fe4d980d8c03
sayaka-takeuchi <ars0915.cute0910@gmail.com>
2021-02-08 23:09:27 +0900

    Add comment

--- src/redux/to-dos.action.ts
+++ src/redux/to-dos.action.ts
@@ -2,6 +2,7 @@ import actionCreatorFactory from "typescript-fsa";
 
 const actionCreator = actionCreatorFactory();
 
-export const toDosAction = {
+// typeが'ADD_TODO'でpayloadがsting型のアクション
+export const toDosAction = {  
   addToDo: actionCreator<string>('ADD_TODO')
 }
--- src/redux/to-dos.reducer.ts
+++ src/redux/to-dos.reducer.ts
@@ -2,15 +2,20 @@ import { reducerWithInitialState } from "typescript-fsa-reducers";
 import { ToDo } from "../models/to-do.model";
 import { toDosAction } from "./to-dos.action";
 
+// Stateの型を指定
 export type ToDosState = {
   toDos: ToDo[];
 }
 
+// 指定した型を用いて初期値を指定
 export const initialToDosState: ToDosState = {
-  toDos:　[],
+  toDos: [],
 };
 
+// 初期値を作成
 export const toDosReducer = reducerWithInitialState(initialToDosState)
+// 状態(state)とアクション(action)を引数にとる(今回は内容がtextのみのためtextになっている)
+// toDosActionのaddTodoの時にreturn内を実行する
   .case(toDosAction.addToDo,(state,text) => {
     state.toDos.push({text})
     return { ...state }
